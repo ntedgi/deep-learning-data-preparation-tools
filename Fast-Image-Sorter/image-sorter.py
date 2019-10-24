cb9ac4b2fbec797a37311d9f9775e1754ff4dff3
naor <naor2205@gmail.com>
2019-10-24 15:51:30 +0300

    getting latest changes

--- Fast-Image-Sorter/image-sorter.py
+++ Fast-Image-Sorter/image-sorter.py
@@ -1,57 +1,96 @@
+import argparse
+import os
+import sys
+from shutil import copyfile
+
 import matplotlib.image as mpimg
 import matplotlib.pyplot as plt
-from shutil import copyfile
-import sys
-import os
-import argparse
 
-parser = argparse.ArgumentParser(description='add path to image directory.')
-parser.add_argument('--dir', dest='accumulate', action='store_const', const=sum, default=max,
-                    help='sum the integers (default: find the max)')
-args = parser.parse_args()
-print(args.accumulate(args.integers))
-
+SORTING_OUTPUT_PATH = "sorter-output"
 
 def mkdir_if_not_exist(path):
     if not os.path.exists(path):
         os.mkdir(path)
 
 
-def read_all_files_inside_dir(path):
+def read_all_files_inside_dir(path, previews_classifications):
     files = []
     for r, d, f in os.walk(path):
         for file in f:
-            if '.jpeg' in file or '.jpg'  in file or '.png' in file:
+            if ('.jpeg' in file or '.jpg' in file) and file not in previews_classifications:
                 files.append(os.path.join(r, file))
     return files
 
 
-def toggle_images(e):
-    if e.key == 'right':
-        print('right')
-        copyfile(image_path, os.path.join(res_folder, 'true', current_image.split("/")[-1]))
-    if e.key == 'left':
-        print('left')
-        copyfile(image_path, os.path.join(res_folder, 'false', current_image.split("/")[-1]))
-    plt.close()
+def get_pictures_allready_been_classified(input_dir):
+    sorted_files_path = f'{input_dir}/{SORTING_OUTPUT_PATH}'
+    result = []
+    if os.path.exists(sorted_files_path):
+        for r, d, f in os.walk(sorted_files_path):
+            for file in f:
+                result.append(file)
+    return result
 
 
-input_dir = sys.argv[1]
-if input_dir[-1] != '/':
-    input_dir += '/'
-res_folder = input_dir + "output"
+class ImageSorter:
+    def __init__(self, input_dir, label):
+        self.current_image_path = ''
+        self.current_image_name = ''
+        self.input_dir = input_dir
+        if input_dir[-1] != '/':
+            self.input_dir += '/'
+        self.output_dir = self.input_dir + SORTING_OUTPUT_PATH
+        self.label = label
+        self.left_click = 0
+        self.right_click = 0
 
-mkdir_if_not_exist(res_folder)
-mkdir_if_not_exist(res_folder + "/true")
-mkdir_if_not_exist(res_folder + "/false")
-current_image = ''
+    def sort(self):
+        previews_classifications = get_pictures_allready_been_classified(self.input_dir)
+        files = read_all_files_inside_dir(self.input_dir, previews_classifications)
+        mkdir_if_not_exist(self.output_dir)
+        mkdir_if_not_exist(f'{self.output_dir}/{self.label}')
+        mkdir_if_not_exist(f'{self.output_dir}/not-{self.label}')
 
-files = read_all_files_inside_dir(input_dir)
-for image_path in files:
-    current_image = image_path
-    image_name = image_path.split("/")[-1]
-    img = mpimg.imread(image_path)
-    plt.title("press right if the picture contains the label else press left.")
-    plt.connect('key_press_event', toggle_images)
-    plt.imshow(img)
-    plt.show()
+        counter = 0
+        for image_path in files:
+            self.current_image_path = image_path
+            self.current_image_name = image_path.split("/")[-1]
+            img = mpimg.imread(image_path)
+            plt.suptitle(f'progress: [ {counter}/{len(files)} ] , {self.label} :{self.right_click}  , not-{self.label} :{self.left_click}')
+            plt.title(f'press right if the picture contains {self.label} else press left.')
+            plt.connect('key_press_event', self.toggle_images)
+            plt.imshow(img)
+            plt.show()
+            counter += 1
+
+    def copy_file_to_dir(self, dir_name):
+        copyfile(self.current_image_path, os.path.join(self.output_dir, dir_name, self.current_image_name))
+
+    def toggle_images(self, e):
+        if e.key == 'right':
+            self.right_click += 1
+            self.copy_file_to_dir(self.label)
+        if e.key == 'left':
+            self.left_click += 1
+            self.copy_file_to_dir(f'not-{self.label}')
+        plt.close()
+
+
+def main():
+    my_parser = argparse.ArgumentParser()
+    my_parser.add_argument('-i', action='store', nargs='+', type=str, required=True)
+    my_parser.add_argument('-l', action='store', type=str, required=True)
+    args = my_parser.parse_args()
+    input_dir = ' '.join(args.i)
+    label = args.l
+
+    if not os.path.isdir(input_dir):
+        print(f'The path : < {input_dir} > does not exist', file=sys.stderr)
+        sys.exit()
+
+    sorter = ImageSorter(input_dir, label)
+    sorter.sort()
+
+
+if __name__ == '__main__':
+    main()
